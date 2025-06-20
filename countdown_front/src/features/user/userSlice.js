42e853899b20fd58588a4f146e37285a0bda986a
ahn <daybreak578@gmail.com>
2025-06-20 12:14:28 +0900

    유저 상태 변경

--- countdown_front/src/features/user/userSlice.js
+++ countdown_front/src/features/user/userSlice.js
@@ -11,6 +11,8 @@ const userSlice = createSlice({
   reducers: {
     setUser: (state, action) => {
       state.user = action.payload;
+      state.username = action.payload.username;
+      state.adminYn = action.payload.adminYn; // 이 줄 필요
     },
     clearUser: (state) => {
       state.user = null;
